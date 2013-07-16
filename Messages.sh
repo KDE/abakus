19b1bd82b9aa84124b910609d4ef5a14c7699e26
Yuri Chornoivan <yurchor@ukr.net>
2013-07-16 08:35:03 +0300

    Parse yy files, do not parse ui and rc (there are no such file in the sources)

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.qml -or -name \*.cpp` -o $podir/abakus.pot
+# $EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
+$XGETTEXT `find . -name \*.qml -or -name \*.cpp -or -name \*.yy` -o $podir/abakus.pot
 rm -f rc.cpp
